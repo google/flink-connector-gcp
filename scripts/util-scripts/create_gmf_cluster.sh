60dd694e589dd983333e4eb1950a05b7815905cf
Iñigo San Jose Visiers <42574310+InigoSJ@users.noreply.github.com>
2024-04-04 09:06:02 -0700

    Add script to create GKE cluster (#1)
    * Add script to create GKE cluster
    
    * Add GMF cluster creation script
    
    * Add GMF cluster creation script
    
    * Fix bug creating cluster and typos

---
+++ scripts/util-scripts/create_gmf_cluster.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Creates GMF cluster and installs Certs and Flink-kubernetes-operator.
+# Command example:
+#   bash scripts/util-scripts/create_gmf_cluster.sh -c <CLUSTER_NAME> -p <PROJECT>  -r <REGION>
+
+# Required parameters
+cluster_name=
+
+# Optional parameters
+region="us-central1"
+project_id=
+
+# Flag parsing with getopts
+while getopts ":c:r:p:" opt; do 
+  case $opt in
+    c) cluster_name=$OPTARG ;; 
+    r) region=$OPTARG ;;
+    p) project_id=$OPTARG ;;
+
+    \?)
+       echo "Invalid option: -$OPTARG" >&2
+       exit 1
+       ;;
+  esac
+done
+
+if [[ ${#cluster_name} -eq 0 ]]; then
+    echo "Error: Missing required parameter -c for cluster name." >&2
+    exit 1  
+fi
+
+PROJECT=""
+if [[ ${#project_id} -ge 1 ]]; then
+   PROJECT="--project=$project_id"
+else
+  echo "Using default project to create cluster"
+fi
+
+
+# Create cluster
+echo "Creating cluster...this may take a few minutes"
+create_cluster=$(gcloud container clusters create-auto $cluster_name --region=$region $PROJECT 2>&1)
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+  if [[ $create_cluster == *"Already exists"* ]]; then
+    echo "Cluster $cluster_name already exists, skipping creation"
+  else
+    echo "Unexpected error occurred: $create_cluster"
+    exit 1
+  fi
+fi
+
+# Install Certs and Flink K8s Operator
+bash $(dirname "$0")/install_certs_and_operator.sh
