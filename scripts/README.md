f3fd1fe826ef80422f311c0b07736d12ad18a15c
Iñigo San Jose Visiers <42574310+InigoSJ@users.noreply.github.com>
2024-04-09 16:49:06 -0700

    Add End-2-End script to create GMF and run Flink deployment (#2)
    * Add script to create GKE cluster
    
    * Add GMF cluster creation script
    
    * Add GMF cluster creation script
    
    * Add create docker image script
    
    * Add e2e scripts
    
    * Fix some bugs
    
    * Fix format
    
    * Fix format
    
    * Fix format
    
    * Fix format
    
    * Fix format
    
    * Fix format
    
    * Include automatic service account. Format
    
    * Add README. Fix pitfall
    
    * Add link
    
    * Fix typos

---
+++ scripts/README.md
@@ -0,0 +1,82 @@
+This is a collection of scripts to create a Google Manage Flink cluster and start a Flink Deployment
+
+# Scripts
+
+The main script `run_flink` creates a GMF cluster, adds needed settings, builds a Docker image and
+starts a Flink Deployment.
+
+There are scripts for each in folder `util-scripts`.
+
+# Parameters
+
+```
+    # Required
+    -c cluster name
+
+    # Optional
+    -a Arguments for the Flink Deployment
+    -e Entry class name for the Flink Deployment
+    -d Name of the deployment
+    -f Flink version, defaults to `1.18.1`
+    -i Full image name, used if you don't want to build the image and pass your own
+    -j Jar URI
+    -m Docker image name
+    -n Repository for docker image
+    -p Project ID
+    -q Parallelism for the Flink Deployment,  defaults to 1
+    -r Region, defaults to `us-central1`
+    -s Service Account used to authentificate the Flink Deployment
+    -t Tag of the Docker image
+
+     # Booleans
+    -B Uses Cloud Build to build the image
+    -C Skips cluster creation
+    -D Skips Docker image creation
+    -F Skips Flink Deployment
+    -M Skips compiling the example pipeline
+    -P Skips installing GCS Plugin
+    -W Skips Workload Indentity Federation annotation
+```
+
+# Examples
+
+## Basic End-to-End
+
+The simplest command only needs the name of the cluster (`-c`), it will use the default Project and
+Compute Engine Service Account (`service-<PROJECT NUMBER>-compute@developer.gserviceaccount.com`).
+
+```
+bash run_flink.sh -c my-cluster
+```
+
+## End-to-End with existing cluster
+
+If you have an existing cluster and want to launch a Flink Deployment using Cloud Build. This will install CERTs, `flink-kubernetes-operator` and annotate the Service Account with [Workloads Identity Federation](https://cloud.google.com/kubernetes-engine/docs/how-to/workload-identity). The script will identify the cluster already exists:
+
+```
+bash run_flink.sh -c my-existing-cluster
+```
+
+If the cluster already has all the needed settings, you can fully skip the creation:
+
+```
+bash run_flink.sh -c my-existing-cluster -C
+```
+
+## End-to-End using an existing image
+
+If you have an existising Docker image and want to use it in a new cluster, you can pass it with parameter `-f`.
+The image has to be available for GMF and the script will skip the creation of a new image:
+
+```
+bash run_flink.sh -c my-cluster -f us-central1-docker.pkg.dev/<PROJECT>/gmf-repo/flink-image:latest -e MyClass -a "--arg1=value1 --arg2 value2" -j "local:///opt/flink/usrlib/my-job.jar"
+```
+
+
+## Create cluster with Service Account, using Cloud Build without Flink Deployment
+
+Creates a GMF cluster with a non-default Service Account and builds a Docker image using Cloud Build
+
+```
+bash run_flink.sh -c my-cluster -s <PROJECT_NUMBER>-compute@developer.gserviceaccount.com -B -F
+```
---
+++ scripts/run_flink.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+# Creates GMF cluster, builds Dockerimage and starts a Flink deployment
+# Command example:
+#   bash scripts/run_flink.sh -c <CLUSTER_NAME> -p <PROJECT>  -r <REGION> -m <IMAGE_NAME> -t <TAG>\
+#       -n <REPO_NAME> -f <FLINK_VERSION> -i <IMAGE> -j <JAR> -d <DEPLOYMENT_NAME> -q <PARALLELISM> \
+#       -f <FLINK_VERSION> -e <ENTRY_CLASS> -a "--arg1 value1 --arg2=value2 --arg3 value3"  \
+#       -B -C -D -F -M -P -W
+
+set -e
+
+# Required parameters
+cluster_name=
+
+# Optional parameters
+region="us-central1"
+flink_version="1.18.1"
+jar_uri="local:///opt/flink/usrlib/gmf-examples.jar"
+deployment_name="wordcount"
+repo_name="gmf-repo"
+image_name="flink-image"
+image_tag="latest"
+entry_class="WordCount"
+parallelism=1
+project_id=
+service_account=
+full_image= 
+args=
+build_maven=true
+create_cluster=true
+create_docker=true
+create_flink_deployment=true
+workload_identity_federation=true
+install_plugins=true
+use_cloud_build=false
+
+# Flag parsing with getopts
+while getopts ":a:c:d:e:f:i:j:m:n:p:q:r:s:t:BCDFMPW" opt; do 
+  case $opt in
+    # Strings
+    a) args=$OPTARG ;;
+    c) cluster_name=$OPTARG ;; 
+    d) deployment_name=$OPTARG ;;
+    e) entry_class=$OPTARG ;; 
+    f) flink_version=$OPTARG ;;
+    i) full_image=$OPTARG ;;
+    j) jar_uri=$OPTARG ;;
+    m) image_name=$OPTARG ;;
+    n) repo_name=$OPTARG ;;
+    p) project_id=$OPTARG ;;
+    r) region=$OPTARG ;;
+    s) service_account=$OPTARG ;;
+    t) image_tag=$OPTARG ;;
+
+    # Integers
+    q) parallelism=$OPTARG ;;
+
+    # Booleans
+    B) use_cloud_build=true ;;
+    C) create_cluster=false ; echo "Skipping Cluster creation" ;;
+    D) create_docker=false ; echo "Skipping creating Docker Image" ;;
+    F) create_flink_deployment=false ; echo "Skipping Flink deployment" ;;
+    M) build_maven=false ;;
+    P) install_plugins=false ;;
+    W) workload_identity_federation=false ; echo "Skipping Workload Identity Federation Annotation" ;;
+
+    \?)
+       echo "Invalid option: -$OPTARG" >&2
+       exit 1
+       ;;
+  esac
+done
+
+if [[ ${#cluster_name} -eq 0 ]]; then
+    echo "Error: Missing required parameter -c for cluster name." >&2
+    exit 1  
+fi
+
+if [[ ${#project_id} -ge 1 ]]; then
+  PROJECT=$project_id
+else
+  echo "Using default project to build image"
+  PROJECT=$(gcloud config get-value project)
+fi
+
+SCRIPT_PATH=$(pwd)/$(dirname "$0")
+
+if [[ $create_cluster == true ]]; then
+  bash "$SCRIPT_PATH"/util-scripts/create_gmf_cluster.sh -c "$cluster_name" \
+  -p "$PROJECT" -r "$region"
+else
+  echo "Getting context for cluster $cluster_name"
+  gcloud container clusters get-credentials "$cluster_name" --region "$region" --project "$PROJECT"
+fi
+
+if [[ $workload_identity_federation == true ]]; then
+  if [[ ${#service_account} -ge 1 ]]; then
+    FULL_SERVICE_ACCOUNT=$service_account
+  else
+    PROJECT_NUMBER=$(gcloud projects describe $PROJECT --format="value(projectNumber)")
+    FULL_SERVICE_ACCOUNT=$PROJECT_NUMBER-compute@developer.gserviceaccount.com
+    echo "Service account parameter (-s) missing, using Compute Engine default service account $FULL_SERVICE_ACCOUNT"
+  fi
+  bash "$SCRIPT_PATH"/util-scripts/annotate_workload_identity_federation.sh -p "$PROJECT" -s "$FULL_SERVICE_ACCOUNT"
+fi
+
+if [[ $create_docker == true ]] && [[ !${#full_image} -ge 1 ]]; then
+  USE_CLOUD_BUILD=""
+  if [[ $use_cloud_build == true ]]; then
+    USE_CLOUD_BUILD="-B"
+  fi
+
+  INSTALL_PLUGINS=""
+  if [[ $install_plugins == false ]]; then
+    INSTALL_PLUGINS="-P"
+  fi
+
+  BUILD_MAVEN=""
+  if [[ $build_maven == false ]]; then
+    BUILD_MAVEN="-M"
+  fi
+
+  bash "$SCRIPT_PATH"/util-scripts/create_docker_image.sh \
+  -r "$region"  -p "$PROJECT"  -f "$flink_version" \
+  -n "$repo_name" -m "$image_name" -t "$image_tag" \
+   $USE_CLOUD_BUILD $BUILD_MAVEN $INSTALL_PLUGINS
+fi
+
+if [[ $create_flink_deployment == true ]]; then
+  if [[ ${#full_image} -ge 1 ]]; then
+    echo "Using custom image $full_image"
+    IMAGE_FULL_NAME=$full_image
+  else
+    IMAGE_FULL_NAME=$region-docker.pkg.dev/$PROJECT/$repo_name/$image_name:$image_tag
+  fi
+
+
+  PARSED_FLINK_VERSION=${flink_version/./_}
+  PARSED_FLINK_VERSION=v${PARSED_FLINK_VERSION%.*}
+  bash "$SCRIPT_PATH"/util-scripts/create_flink_deployment.sh -i "$IMAGE_FULL_NAME" \
+   -j "$jar_uri" -f "$PARSED_FLINK_VERSION" \
+   -d "$deployment_name" -a "$args" \
+   -e "$entry_class" -q $parallelism
+fi
