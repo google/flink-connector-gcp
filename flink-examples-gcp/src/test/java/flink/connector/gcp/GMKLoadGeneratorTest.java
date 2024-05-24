acd3996b37f9e1c167592ad1fd6dd9a4616a7023
EllaDing <yd2419@columbia.edu>
2024-05-24 18:53:45 +0000

    Remove unncessary comment.

--- flink-examples-gcp/src/test/java/flink/connector/gcp/GMKLoadGeneratorTest.java
+++ flink-examples-gcp/src/test/java/flink/connector/gcp/GMKLoadGeneratorTest.java
@@ -31,7 +31,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-// import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /** Testing data generation logic. */
