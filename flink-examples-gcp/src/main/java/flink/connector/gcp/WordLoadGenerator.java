3d159ae89cc35724556d94e2a1d7176f23bc148d
edman124 <chengedward@google.com>
2024-05-24 00:18:56 +0000

    read words.txt from gcs

--- flink-examples-gcp/src/main/java/flink/connector/gcp/WordLoadGenerator.java
+++ flink-examples-gcp/src/main/java/flink/connector/gcp/WordLoadGenerator.java
@@ -21,9 +21,9 @@ package flink.connector.gcp;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.util.Collector;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 
 /** Creates load from a set list of words. */
 public class WordLoadGenerator implements FlatMapFunction<Long, String> {
@@ -32,21 +32,17 @@ public class WordLoadGenerator implements FlatMapFunction<Long, String> {
 
     public WordLoadGenerator(int l) {
         load = l;
-        String resourcePath = "words.txt"; // No leading slash
+        try {
+            StorageOptions options = StorageOptions.newBuilder()
+                    .setProjectId("managed-flink-shared-dev").build();
 
-        // Using the ClassLoader to load the resource
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        System.out.println(System.getProperty("java.class.path"));
-        try (InputStream inputStream = classLoader.getResourceAsStream(resourcePath)) {
-            if (inputStream == null) {
-                throw new IOException("Resource not found: " + resourcePath);
-            }
+            Storage storage = options.getService();
+            Blob blob = storage.get("gmf-samples", "words.txt");
 
             // Read the InputStream into a byte array
-            wordStr = inputStream.readAllBytes();
+            wordStr = blob.getContent(null);
 
-        } catch (IOException e) {
-            System.out.println(new File("").getAbsolutePath());
+        } catch (Exception e) {
             System.out.println("Could not read words.txt file, using the head of the file instead");
             wordStr =
                     ("Release, Ornamental, Cosmetic, Cement, Mud, Cleave, Zephyr, "
@@ -54,7 +50,7 @@ public class WordLoadGenerator implements FlatMapFunction<Long, String> {
                                     + "Tramp, Heavyset, Current, Whisper, Alert, Approval, Forsake, Wind, "
                                     + "Consult, Women, Pitch, Easier, Shirk, Fighter, Disastrous, Basis, "
                                     + "Vanish, Freezing, Soar, Old-fashioned, Blankly, Closed, Parade, "
-                                    + "Prophetic, Sponge, Moldy")
+                                    + "Prophetic, Sponge, Moldy,  Ransom, Identical, Tremor, Bearskin, Shout, Hook, Blackheart, Travel, Quit, Faulty, Cheerful, Dope, Cast, Admiral, Pen, Phenomenal, Acquire, Bird, Gentle, Selective, Bone, Scissors, Knowing, Begin, Guidebook, Stupid, Insurance, Conflict, Lace, Rule, Righteous, Warrior, Hundred, Coercion, Wiry, Aroma, Absorb, Homicide, Curve, Hammer, Fireman, Jump, Fake, Communication, Glance, Gushing, Ferment, Propellant, Enter, Kangaroo, Harplike, Scenic, Economics, Ignorant, Dirt, Elimination, Dozen, Dear, Mew, Vomit, Eruption, Reception, Arch, Security, Dynamite, Cynical, Murmur, Machine, Educated, Eternity, Manufacturer, Audience, Engine, Granite, Icicle, Loose, Guilty, Driving, Afraid, Crowd, Sugar, Harmonious, Airplane, Rerun, Fabulous, Balloon, Giddy, Smut, Bronze, Curfew, Harmonic, Death, Blimp, Calculate, Decade, Satisfy, Virus, Banana, Manage, Grand, Appetite, Celebrate, Beeswax, Recording, Dime, Speed, Round, Dignitary, Contemn, Serum, Pet, Absent, Confidence, Crispy, Hangman, Collide, Guilt, Cannibalism, Berserk, Agreement, Implant, Sable, Sass, Bludgeon, Construe, Indulge, Grin, Crazy, Boring, Spider, Animatronic, Stealthy, Discipline, Slimy, Quince, Blooper, Plucky, Civilization, Morning, Pandemic, Front, Wood, Industry, Makeshift, Zesty, World, Fugitive, Reflective, Horses, Baby, Tall, Adopter, Puffy, Squeeze, Baseball, Minipill, Dazzling, Aftertaste, Omniscient, Purring, Abducted, Retain, Essence, Link, Piano, Courage, Identify, Purpose, Whole, Appearance, Chain, Shrill, Alcohol, Sadistic, Erase, Entropy, Flight, Endorse, Crisp, Digest, Bed, Brass, Rat, Gainsay, Stir, Drop, Spiders, Blankness, Diplomat, Republic, Circuitry, Sphere, Honest, Augmentation, Cemetery, Recite, Bloke, Rustic, Hairless, Closing, Belligerent, Devastation, Religion, Inner, Pony, Left, Imitate, Falling, Domineering, Cigarette, Benevolent, Crafty, Affair, Freak, Alleged, Mice, Wish, Council, Animal, Contamination, Pick, Abnormality, Entity, Carnival, Youth, Press, Jade, Torpedo, Tend, Analyst, Uptown, Call, Far-flung,")
                             .getBytes();
             e.printStackTrace();
         }
