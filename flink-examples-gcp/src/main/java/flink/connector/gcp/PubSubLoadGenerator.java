d186d39d9b8ee602cdb87dad4512fee61a9d9aa9
clmccart <clairemccarthy@google.com>
2024-06-03 13:54:33 -0700

    add load generator filter to all the load gen pipelines (#27)
    * add load gen filter to gcs load gen
    
    * add load filter to pubsub load gen

--- flink-examples-gcp/src/main/java/flink/connector/gcp/PubSubLoadGenerator.java
+++ flink-examples-gcp/src/main/java/flink/connector/gcp/PubSubLoadGenerator.java
@@ -29,12 +29,15 @@ import org.apache.flink.configuration.StateBackendOptions;
 import org.apache.flink.connector.datagen.source.DataGeneratorSource;
 import org.apache.flink.connector.datagen.source.GeneratorFunction;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import com.google.pubsub.flink.PubSubSerializationSchema;
 import com.google.pubsub.flink.PubSubSink;
 
+import java.time.Clock;
 import java.time.Duration;
+import java.util.Random;
 
 /** Creates load with pseudo random bytes and sends to the output PubSub topic. */
 public class PubSubLoadGenerator {
@@ -51,7 +54,9 @@ public class PubSubLoadGenerator {
         String projectName = parameters.get("projectName", "managed-flink-shared-dev");
         int load = parameters.getInt("messageSizeKB", 10);
         int rate = parameters.getInt("messagesPerSecond", 1000);
-        System.out.println(String.format("Message load: %d; Rate Per Sec: %d", load, rate));
+        Long loadPeriod = parameters.getLong("load-period-in-second", 3600);
+        String pattern = parameters.get("pattern", "static");
+        System.out.println(String.format("Message load: %d; Rate Per Sec: %d, Load pattern: %s, Load period: %d", load, rate, pattern, loadPeriod));
 
         // Add checkpointing, this is needed for files to leave the "in progress state"
         Configuration config = new Configuration();
@@ -80,7 +85,9 @@ public class PubSubLoadGenerator {
         DataStreamSource<Long> generator =
                 env.fromSource(generatorSource, WatermarkStrategy.noWatermarks(), "Data Generator");
 
-        generator.flatMap(new WordLoadGenerator(load * KB)).sinkTo(sink).uid("writer");
+        // Apply the input load filter.
+        SingleOutputStreamOperator<Long> filteredGenerator = generator.filter(new InputLoadFilter(loadPeriod, pattern, Clock.systemDefaultZone(), new Random())).uid(pattern.concat(" filter"));
+        filteredGenerator.flatMap(new WordLoadGenerator(load * KB)).sinkTo(sink).uid("writer");
 
         env.execute("Write to PubSub Unbounded");
     }
