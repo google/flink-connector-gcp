f330bf0a07bf2fb315771e4863fb8bf022c859ed
edman124 <chengedward@google.com>
2024-06-18 23:02:39 +0000

    create gmk to gmk pipeline (plain sasl)

---
+++ flink-examples-gcp/src/main/java/flink/connector/gcp/GMKToGMKWordCount.java
@@ -0,0 +1,110 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package flink.connector.gcp;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.MultipleParameterTool;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+import org.apache.avro.Schema;
+
+/** Pipeline code for running word count reading from GMK and writing to GMK. */
+public class GMKToGMKWordCount {
+    static Schema schema;
+
+    public static void main(String[] args) throws Exception {
+        final MultipleParameterTool parameters = MultipleParameterTool.fromArgs(args);
+        String brokers = parameters.get("brokers", "localhost:9092");
+        String gmkUsername = parameters.get("gmk-username");
+        String kafkaTopic = parameters.get("kafka-topic", "my-topic");
+        String kafkaSinkTopic = parameters.get("kafka-sink-topic", "sink-topic");
+        String projectId = parameters.get("project-id");
+        Long checkpointInterval = parameters.getLong("checkpoint-interval", 60000L);
+        String jobName = parameters.get("job-name", "GMK-GMK-word-count");
+        System.out.println("Starting job ".concat(jobName));
+        Configuration conf = new Configuration();
+        conf.setString("restart-strategy.type", "fixed-delay");
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
+        env.getConfig().setGlobalJobParameters(parameters);
+        env.enableCheckpointing(checkpointInterval);
+
+        KafkaSource<String> source =
+                KafkaSource.<String>builder()
+                        .setBootstrapServers(brokers)
+                        .setTopics(kafkaTopic)
+                        .setStartingOffsets(OffsetsInitializer.earliest())
+                        .setValueOnlyDeserializer(new SimpleStringSchema())
+                        .setProperty("partition.discovery.interval.ms", "10000")
+                        .setProperty("security.protocol", "SASL_SSL")
+                        .setProperty("sasl.mechanism", "PLAIN")
+                        .setProperty(
+                                "sasl.jaas.config",
+                                String.format(
+                                        "org.apache.kafka.common.security.plain.PlainLoginModule required username=\'%s\' password=\"%s\";",
+                                        gmkUsername, System.getenv("GMK_PASSWORD")))
+                        .build();
+
+        KafkaSink<String> sink =
+                KafkaSink.<String>builder()
+                        .setBootstrapServers(brokers)
+                        .setRecordSerializer(KafkaRecordSerializationSchema.builder()
+                            .setTopic(kafkaSinkTopic)
+                            .setValueSerializationSchema(new SimpleStringSchema())
+                            .build()
+                        )
+                        .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                        .build();
+
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
+                .flatMap(new PrepareWC())
+                .keyBy(tuple -> tuple.f0)
+                .sum(1)
+                .map(
+                    tup -> {
+                        return String.format("word: %s count: %s", tup.f0, tup.f1);
+                    })
+                .sinkTo(sink).name(jobName + "-sink");
+
+        env.execute(jobName);
+    }
+
+    /** Splits tokens. */
+    public static final class PrepareWC
+            implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            for (String split : value.split(" ")) {
+                if (!split.equals(",") && !split.isEmpty()) {
+                    out.collect(new Tuple2<>(split.toLowerCase(), 1));
+                }
+            }
+        }
+    }
+}
