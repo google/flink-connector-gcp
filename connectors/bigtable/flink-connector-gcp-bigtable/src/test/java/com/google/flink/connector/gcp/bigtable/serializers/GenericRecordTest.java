6221ad9b27e698e5042152ebb0835c0c95e043e8
Iñigo San Jose Visiers <42574310+InigoSJ@users.noreply.github.com>
2024-11-15 15:47:09 -0800

    Add nested row mode to the Generic Record Serializer (#67)
    * Add nested row mode
    
    * Add clarification to doc about cf mode
    
    * Fix import
    
    * Modify serializer init test. Add error to ErrorMessages
    
    * remove getters. Fix typos

--- connectors/bigtable/flink-connector-gcp-bigtable/src/test/java/com/google/flink/connector/gcp/bigtable/serializers/GenericRecordTest.java
+++ connectors/bigtable/flink-connector-gcp-bigtable/src/test/java/com/google/flink/connector/gcp/bigtable/serializers/GenericRecordTest.java
@@ -20,11 +20,15 @@ package com.google.flink.connector.gcp.bigtable.serializers;
 
 import com.google.cloud.bigtable.data.v2.models.RowMutationEntry;
 import com.google.flink.connector.gcp.bigtable.testingutils.TestingUtils;
+import com.google.flink.connector.gcp.bigtable.utils.ErrorMessages;
 import org.apache.avro.Schema;
 import org.apache.avro.SchemaBuilder;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.nio.ByteBuffer;
 
@@ -40,15 +44,78 @@ import static org.junit.Assert.assertEquals;
  */
 public class GenericRecordTest {
 
-    @Test
-    public void testCorrectRowMutationSerialization() {
-        GenericRecordToRowMutationSerializer serializer = createTestSerializer();
-        RowMutationEntry wantedEntry = TestingUtils.getTestRowMutationEntry();
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void testCorrectSerializerInitialization(Boolean useNestedRowsMode) {
+        GenericRecordToRowMutationSerializer.Builder builder =
+                GenericRecordToRowMutationSerializer.builder()
+                        .withRowKeyField(TestingUtils.ROW_KEY_FIELD);
+        if (useNestedRowsMode) {
+            builder.withNestedRowsMode();
+        } else {
+            builder.withColumnFamily(TestingUtils.COLUMN_FAMILY);
+        }
+        GenericRecordToRowMutationSerializer serializer = builder.build();
 
-        GenericRecord record = getTestGenericRecord();
+        assertEquals(TestingUtils.ROW_KEY_FIELD, serializer.rowKeyField);
+        assertEquals(
+                useNestedRowsMode ? null : TestingUtils.COLUMN_FAMILY, serializer.columnFamily);
+        assertEquals(useNestedRowsMode, serializer.useNestedRowsMode);
+    }
+
+    @Test
+    public void testColumnFamilyAndNestedIncompability() {
+        Assertions.assertThatThrownBy(
+                        () ->
+                                GenericRecordToRowMutationSerializer.builder()
+                                        .withRowKeyField(TestingUtils.ROW_KEY_FIELD)
+                                        .withNestedRowsMode()
+                                        .withColumnFamily(TestingUtils.COLUMN_FAMILY)
+                                        .build())
+                .hasMessageContaining(ErrorMessages.COLUMN_FAMILY_AND_NESTED_INCOMPATIBLE);
+    }
+
+    @Test
+    public void testNotColumnFamilyOrNestedRequired() {
+        Assertions.assertThatThrownBy(
+                        () ->
+                                GenericRecordToRowMutationSerializer.builder()
+                                        .withRowKeyField(TestingUtils.ROW_KEY_FIELD)
+                                        .build())
+                .hasMessageContaining(ErrorMessages.COLUMN_FAMILY_OR_NESTED_ROWS_REQUIRED);
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void testCorrectRowMutationSerialization(Boolean useNestedRowsMode) {
+        GenericRecordToRowMutationSerializer serializer = createTestSerializer(useNestedRowsMode);
+        RowMutationEntry wantedEntry = TestingUtils.getTestRowMutationEntry(useNestedRowsMode);
+
+        GenericRecord record =
+                useNestedRowsMode ? getTestGenericRecordNested() : getTestGenericRecord();
         RowMutationEntry serializedEntry = serializer.serialize(record, null);
 
         TestingUtils.assertRowMutationEntryEquality(serializedEntry, wantedEntry);
+    }
+
+    @Test
+    public void testWrongSchemaNestedRowsMode() {
+        GenericRecordToRowMutationSerializer serializer = createTestSerializer(true);
+
+        GenericRecord record = getTestGenericRecord();
+
+        Assertions.assertThatThrownBy(() -> serializer.serialize(record, null))
+                .hasMessageContaining(ErrorMessages.BASE_NO_NESTED_TYPE);
+    }
+
+    @Test
+    public void testDoubleNestedRowsError() {
+        GenericRecordToRowMutationSerializer serializer = createTestSerializer(true);
+
+        GenericRecord testRecord = getTestGenericRecordDoubleNested();
+
+        Assertions.assertThatThrownBy(() -> serializer.serialize(testRecord, null))
+                .hasMessageContaining(ErrorMessages.UNSUPPORTED_SERIALIZATION_TYPE);
     }
 
     @Test
@@ -85,12 +152,15 @@ public class GenericRecordTest {
         }
     }
 
-    private GenericRecordToRowMutationSerializer createTestSerializer() {
+    private GenericRecordToRowMutationSerializer createTestSerializer(Boolean useNestedRowsMode) {
         GenericRecordToRowMutationSerializer.Builder builder =
                 GenericRecordToRowMutationSerializer.builder()
-                        .withRowKeyField(TestingUtils.ROW_KEY_FIELD)
-                        .withColumnFamily(TestingUtils.COLUMN_FAMILY);
-
+                        .withRowKeyField(TestingUtils.ROW_KEY_FIELD);
+        if (useNestedRowsMode) {
+            builder.withNestedRowsMode();
+        } else {
+            builder.withColumnFamily(TestingUtils.COLUMN_FAMILY);
+        }
         return builder.build();
     }
 
@@ -110,6 +180,88 @@ public class GenericRecordTest {
         testRecord.put(TestingUtils.STRING_FIELD, TestingUtils.STRING_VALUE);
         testRecord.put(TestingUtils.INTEGER_FIELD, TestingUtils.INTEGER_VALUE);
 
+        return testRecord;
+    }
+
+    private static GenericRecord getTestGenericRecordNested() {
+        Schema nestedSchema1 =
+                SchemaBuilder.builder()
+                        .record(TestingUtils.NESTED_COLUMN_FAMILY_1)
+                        .fields()
+                        .requiredString(TestingUtils.STRING_FIELD)
+                        .requiredInt(TestingUtils.INTEGER_FIELD)
+                        .endRecord();
+
+        Schema nestedSchema2 =
+                SchemaBuilder.builder()
+                        .record(TestingUtils.NESTED_COLUMN_FAMILY_2)
+                        .fields()
+                        .requiredString(TestingUtils.STRING_FIELD_2)
+                        .endRecord();
+
+        Schema schema =
+                SchemaBuilder.builder()
+                        .record("Test")
+                        .fields()
+                        .requiredString(TestingUtils.ROW_KEY_FIELD)
+                        .name(TestingUtils.NESTED_COLUMN_FAMILY_1)
+                        .type(nestedSchema1)
+                        .noDefault()
+                        .name(TestingUtils.NESTED_COLUMN_FAMILY_2)
+                        .type(nestedSchema2)
+                        .noDefault()
+                        .endRecord();
+
+        GenericRecord testRecord = new GenericData.Record(schema);
+        GenericRecord nestedRecord1 = new GenericData.Record(nestedSchema1);
+        GenericRecord nestedRecord2 = new GenericData.Record(nestedSchema2);
+
+        nestedRecord1.put(TestingUtils.STRING_FIELD, TestingUtils.STRING_VALUE);
+        nestedRecord1.put(TestingUtils.INTEGER_FIELD, TestingUtils.INTEGER_VALUE);
+
+        nestedRecord2.put(TestingUtils.STRING_FIELD_2, TestingUtils.STRING_VALUE_2);
+
+        testRecord.put(TestingUtils.ROW_KEY_FIELD, TestingUtils.ROW_KEY_VALUE);
+        testRecord.put(TestingUtils.NESTED_COLUMN_FAMILY_1, nestedRecord1);
+        testRecord.put(TestingUtils.NESTED_COLUMN_FAMILY_2, nestedRecord2);
+
+        return testRecord;
+    }
+
+    private static GenericRecord getTestGenericRecordDoubleNested() {
+        Schema doubleNestSchema =
+                SchemaBuilder.builder()
+                        .record("doubleNest")
+                        .fields()
+                        .requiredString(TestingUtils.STRING_FIELD)
+                        .endRecord();
+        Schema nestSchema =
+                SchemaBuilder.builder()
+                        .record("nest")
+                        .fields()
+                        .name("doubleNest")
+                        .type(doubleNestSchema)
+                        .noDefault()
+                        .endRecord();
+        Schema schema =
+                SchemaBuilder.builder()
+                        .record("Test")
+                        .fields()
+                        .requiredString(TestingUtils.ROW_KEY_FIELD)
+                        .name("nest")
+                        .type(nestSchema)
+                        .noDefault()
+                        .endRecord();
+
+        GenericRecord testRecord = new GenericData.Record(schema);
+        GenericRecord nest = new GenericData.Record(nestSchema);
+        GenericRecord doubleNest = new GenericData.Record(doubleNestSchema);
+
+        doubleNest.put(TestingUtils.STRING_FIELD, TestingUtils.STRING_VALUE);
+        nest.put("doubleNest", doubleNest);
+
+        testRecord.put(TestingUtils.ROW_KEY_FIELD, TestingUtils.ROW_KEY_VALUE);
+        testRecord.put("nest", nest);
         return testRecord;
     }
 
---
+++ connectors/bigtable/flink-examples-gcp-bigtable/src/main/java/com/google/flink/connector/gcp/bigtable/examples/WriteGenericRecordNested.java
@@ -0,0 +1,157 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.flink.connector.gcp.bigtable.examples;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.connector.datagen.source.DataGeneratorSource;
+import org.apache.flink.connector.datagen.source.GeneratorFunction;
+import org.apache.flink.formats.avro.typeutils.GenericRecordAvroTypeInfo;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import com.google.flink.connector.gcp.bigtable.BigtableSink;
+import com.google.flink.connector.gcp.bigtable.serializers.GenericRecordToRowMutationSerializer;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+
+import java.nio.ByteBuffer;
+
+/**
+ * This is an example pipeline for Apache Flink that demonstrates writing data to Google Cloud
+ * Bigtable using the Bigtable connector and a {@link GenericRecordToRowMutationSerializer} with
+ * Nested Rows to transform data into {@link RowMutationEntry} objects.
+ *
+ * <p>The pipeline generates a stream of Long values and uses a {@link
+ * GenericRecordToRowMutationSerializer} to convert each Long value into a {@link RowMutationEntry}
+ * that can be written to Bigtable.
+ *
+ * <p>You can run this example by passing the following command line arguments:
+ *
+ * <pre>
+ *   --instance &lt;bigtable instance id&gt; \
+ *   --project &lt;gcp project id&gt; \
+ *   --table &lt;bigtable table id&gt; \
+ *   --rate &lt;number of rows to generate per second&gt; \
+ *   --jobName &lt;job name&gt;
+ * </pre>
+ */
+public class WriteGenericRecordNested {
+
+    public static void main(String[] args) throws Exception {
+        final ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String instance = parameterTool.get("instance");
+        String project = parameterTool.get("project");
+        String table = parameterTool.get("table");
+        Integer rate = parameterTool.getInt("rate", 10000);
+        String jobName =
+                parameterTool.get("jobName", "Streaming Bigtable Write GenericRecord Nested");
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        GeneratorFunction<Long, Long> generatorFunction = n -> n;
+        DataGeneratorSource<Long> generatorSource =
+                new DataGeneratorSource<>(
+                        generatorFunction,
+                        Long.MAX_VALUE,
+                        RateLimiterStrategy.perSecond(rate),
+                        Types.LONG);
+
+        DataStreamSource<Long> generator =
+                env.fromSource(generatorSource, WatermarkStrategy.noWatermarks(), "Data Generator");
+
+        Schema schema =
+                SchemaBuilder.record("User")
+                        .fields()
+                        .requiredString("key")
+                        .name("family1")
+                        .type(
+                                SchemaBuilder.record("Family1")
+                                        .fields()
+                                        .requiredLong("ageLong")
+                                        .requiredDouble("ageDouble")
+                                        .requiredFloat("ageFloat")
+                                        .endRecord())
+                        .noDefault()
+                        .name("family2")
+                        .type(
+                                SchemaBuilder.record("Family2")
+                                        .fields()
+                                        .requiredBytes("textBytes")
+                                        .requiredBoolean("isActive")
+                                        .endRecord())
+                        .noDefault()
+                        .endRecord();
+
+        generator
+                .map(new ToNestedGenericRecord(schema))
+                .returns(new GenericRecordAvroTypeInfo(schema))
+                .sinkTo(
+                        BigtableSink.<GenericRecord>builder()
+                                .setProjectId(project)
+                                .setInstanceId(instance)
+                                .setTable(table)
+                                .setSerializer(
+                                        GenericRecordToRowMutationSerializer.builder()
+                                                .withRowKeyField("key")
+                                                .withNestedRowsMode()
+                                                .build())
+                                .build())
+                .name("BigtableSink");
+
+        env.execute(jobName);
+    }
+
+    /** Convert to GenericRecord. */
+    public static final class ToNestedGenericRecord implements MapFunction<Long, GenericRecord> {
+        Schema schema;
+
+        public ToNestedGenericRecord(Schema schema) {
+            this.schema = schema;
+        }
+
+        @Override
+        public GenericRecord map(Long l) throws Exception {
+            GenericRecord user = new GenericData.Record(schema);
+            GenericRecord family1 = new GenericData.Record(schema.getField("family1").schema());
+            GenericRecord family2 = new GenericData.Record(schema.getField("family2").schema());
+
+            // Generate a non-lexicographically-sorted unique key
+            String key = String.format("%d#%d#%d#%d", l % 11, l % 101, l % 1013, l);
+
+            family1.put("ageLong", l);
+            family1.put("ageDouble", l.doubleValue());
+            family1.put("ageFloat", l.floatValue() / 3.3f);
+
+            family2.put("textBytes", ByteBuffer.wrap(key.getBytes()));
+            family2.put("isActive", l % 2 == 0);
+
+            user.put("key", key);
+            user.put("family1", family1);
+            user.put("family2", family2);
+
+            return user;
+        }
+    }
+}
